68e032be0407c0d271919852c44343d5e7709fdd
PRASEN JEET <theprasenjeet@gmail.com>
2023-04-05 19:14:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import pickle
 import streamlit as st
+import numpy as np
 
 # Load the model
 with open('phishing_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Create the Streamlit app
-st.write('# Phishing Website Prediction')
+# Define the Streamlit app
+def app():
+    st.set_page_config(page_title="Phishing Website Detector", page_icon=":guardsman:", layout="centered")
 
-url = st.text_input('Enter a website URL')
+    st.title("Phishing Website Detector")
 
-if st.button('Predict'):
-    # Make a prediction
-    prediction = model.predict([url])[0]
+    url = st.text_input("Enter the website URL:")
 
-    # Show the prediction
-    if prediction == 'good':
-        st.write('This website is safe')
-    else:
-        st.write('This website is a phishing website')
+    if st.button("Predict"):
+        # Reshape the input data to have shape (1, )
+        input_data = np.array([url]).reshape(1, )
+
+        # Make the prediction
+        prediction = model.predict(input_data)[0]
+
+        # Show the prediction
+        if prediction == 'good':
+            st.success("This website is safe!")
+        else:
+            st.warning("This website is a phishing website!")
