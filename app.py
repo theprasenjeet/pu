f465ba30c3a81164eee506b4c2ddd6ae4bfd734f
PRASEN JEET <theprasenjeet@gmail.com>
2023-04-05 15:30:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
-import requests
+import pickle
+import pandas as pd
 import re
+
+# Load the pre-trained model from phishing.pkl
+with open('phishing.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 # Define the Streamlit app
 st.title('Phishing Detector')
@@ -9,41 +14,38 @@ st.write('Enter a URL to check if it is a phishing website or not.')
 # Define the input field for the user to enter a URL
 input_url = st.text_input('URL')
 
-# Define a function to check if a URL is a phishing website
-def is_phishing(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        return False
-
-    html_content = response.content.decode('utf-8')
-    if 'content="text/html' not in html_content:
-        return False
-
-    # check if the url contains an IP address
-    if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', url):
-        return True
-
-    # check for common phishing keywords in the url
-    keywords = ['login', 'signin', 'bank', 'paypal', 'ebay', 'amazon']
-    for keyword in keywords:
-        if keyword in url:
-            return True
-
-    # check for redirection to a different domain
-    if response.history:
-        last_url = response.history[-1].url
-        if last_url != url:
-            return True
-
-    return False
+# Define a function to preprocess the input URL
+def preprocess_url(url):
+    # Remove any unnecessary parts of the URL
+    url = re.sub(r'https?://', '', url)
+    url = re.sub(r'www\.', '', url)
+    url = re.sub(r'\/.*', '', url)
+    
+    # Tokenize the URL
+    tokens = re.findall(r'\w+', url)
+    
+    # Stem the tokens
+    stemmed_tokens = [token.lower() for token in tokens]
+    
+    # Convert the tokens to a string
+    token_str = ' '.join(stemmed_tokens)
+    
+    # Convert the tokens to a pandas DataFrame
+    data = {'text_tokenized': [tokens], 'text_stemmed': [stemmed_tokens], 'text_sent': [[]]}
+    df = pd.DataFrame(data)
+    
+    return df
 
 # Perform phishing detection when the user clicks the button
 if st.button('Check'):
-    # Check if the input URL is a phishing website
-    result = is_phishing(input_url)
+    # Preprocess the input URL
+    input_df = preprocess_url(input_url)
+    
+    # Use the pre-trained model to predict if the URL is a phishing website or not
+    result = model.predict(input_df)[0]
     
     # Display the phishing detection result to the user
-    if result:
+    if result == 1:
         st.write('The URL is a phishing website.')
     else:
         st.write('The URL is not a phishing website.')
