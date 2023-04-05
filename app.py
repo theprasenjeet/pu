8ca429635000268a88fb53fe3352cca6bf3ddf60
PRASEN JEET <theprasenjeet@gmail.com>
2023-04-05 19:09:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,21 @@
+import pickle
 import streamlit as st
-import requests
-import os
 
-# Get the FraudGuard API username and password from environment variables
-API_USERNAME = os.environ.get("6hklAl7a1g4TLdCE")
-API_PASSWORD = os.environ.get("X8C9Ifk1DWTdh1oX")
+# Load the model
+with open('phishing_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
-# Define the API endpoint URL and parameters
-API_URL = "https://api.fraudguard.io/v1/scan"
-API_PARAMS = {"url": None}
+# Create the Streamlit app
+st.write('# Phishing Website Prediction')
 
-st.title("Phishing Website Detector")
+url = st.text_input('Enter a website URL')
 
-# Get the user input URL
-url = st.text_input("Enter a website URL:")
+if st.button('Predict'):
+    # Make a prediction
+    prediction = model.predict([url])[0]
 
-# Check if the user has entered a URL
-if url:
-    # Update the API parameters with the user input URL
-    API_PARAMS["url"] = url
-    
-    # Call the FraudGuard API and get the response
-    response = requests.post(API_URL, data=API_PARAMS, auth=(API_USERNAME, API_PASSWORD))
-    
-    # Check if the API call was successful
-    if response.ok:
-        # Parse the JSON response and get the result
-        result = response.json()
-        
-        # Check if the URL is a phishing website or not
-        if result["status"] == "malicious":
-            st.error("The URL is a phishing website.")
-        else:
-            st.success("The URL is not a phishing website.")
+    # Show the prediction
+    if prediction == 'good':
+        st.write('This website is safe')
     else:
-        st.error("An error occurred while connecting to the FraudGuard API.")
+        st.write('This website is a phishing website')
